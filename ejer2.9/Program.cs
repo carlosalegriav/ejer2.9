afa1b649e672d84904e0fcc25f82ba3a0deac35b
Lenovo <Lenovo@DESKTOP-620RG2R>
2022-11-13 21:56:36 -0500

    Agregar archivos de proyecto.

---
+++ ejer2.9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejerciciodosnueve
+{
+    public class sueldo
+    {
+        public static void Main()
+        {
+            int SS, CH, VH;
+            Console.WriteLine("Dijite las horas trabajadas");
+            CH= int.Parse(Console.ReadLine());
+            Console.WriteLine("Dijite el valor de la hora");
+            VH= int.Parse(Console.ReadLine());
+            SS = CH * VH;
+            Console.WriteLine($"El valor de la semana es : {SS}");
+
+        }
+    }
+}
